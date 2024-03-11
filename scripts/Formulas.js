588b4420a8c6735061573db13660dedf326c22ea
MeatSpin360 <emiliobrescasin@gmail.com>
2024-03-11 17:00:34 -0300

    Empezando las formulas en js

--- scripts/Formulas.js
+++ scripts/Formulas.js
@@ -1,14 +1,37 @@
-// aca van a ir las cosas para practicar
+// aca van funciones varias y variables
 
-const botoncalcular = document.querySelector('#calcular')
-
-botoncalcular.onclick = function(){
-    alert('ahora te calculamos');
-
+function multiplicar(argu1, argu2) {
+    return (argu1 * argu2)
 };
 
-let NombreUsuario = document.querySelector('#NombreUsuario');
+function dividir(argu1, argu2) {
+    return (argu1 / argu2)
+};
 
-console.log(NombreUsuario);
+function sumar(argu1, argu2) {
+    return (argu1 + argu2)
+};
 
+function restar(argu1, argu2) {
+    return(argu1 - argu2)
+};
+
+// Aca arranca la funcion principal
+
+const $botoncalcular = document.querySelector('#calcular')
+
+$botoncalcular.onclick = function(){
+    alert('ahora te calculamos');
+
+    let NombreUsuario = document.querySelector('#NombreUsuario').value;
+
+    let datos = {document.querySelector('#basico').value, document.querySelector('#acfa').value, document.querySelector('#conformado').value, document.querySelector('#antiguedad').value, document.querySelector('#feriado').value, document.querySelector('#horas50').value, document.querySelector('#horas100').value}
+
+for
+
+    let respuestadeerror;
+
+
+    document.querySelector('#respuestadeerror').innerText = respuestadeerror
+};
 
