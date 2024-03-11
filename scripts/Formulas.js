2ac03f250638d47c135fbdb18ae6f83d3ac297a9
MeatSpin360 <emiliobrescasin@gmail.com>
2024-03-11 15:40:27 -0300

    avance de estructura general, boton funcional

--- scripts/Formulas.js
+++ scripts/Formulas.js
@@ -1,3 +1,14 @@
 // aca van a ir las cosas para practicar
 
-console.log(null || 'string')
+const botoncalcular = document.querySelector('#calcular')
+
+botoncalcular.onclick = function(){
+    alert('ahora te calculamos');
+
+};
+
+let NombreUsuario = document.querySelector('#NombreUsuario');
+
+console.log(NombreUsuario);
+
+
